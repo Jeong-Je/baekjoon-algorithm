f5e45a5ba3c030240aa778a326a4a773af3222d4
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-05-10 22:34:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,23 @@
+//10818
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N,num;
+	int max = -1000000, min = 1000000;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		if (num > max)max = num;
+		if (num < min)min = num;
+	}
+
+	cout << min << " " << max;
+
+
+
+	return 0;
+}
