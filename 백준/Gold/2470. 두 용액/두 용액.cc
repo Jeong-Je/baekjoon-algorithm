d91e31d936d28dbb2b255a980cc85d7048cd8157
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-10-10 22:52:21 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,49 @@
+// 2470
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++) 
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	int start = 0;
+	int end = n - 1;
+
+	int min = 2000000000;
+	
+	vector<int> result(2);
+
+	while (start < end) {
+		int sum = v.at(start) + v.at(end);
+
+		if (abs(sum) < min) {
+			min = abs(sum);
+			result[0] = v.at(start);
+			result[1] = v.at(end);
+
+			if (min == 0)break;
+		}
+
+		if (sum < 0)
+			start++;
+		else
+			end--;
+	}
+
+	cout << result.at(0) << " " << result.at(1);
+
+	return 0;
+}
