82e33351dcd8ae8e746a9fab70c24d8ab1e7e2bd
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-11-12 22:55:04 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	long n;
+	cin >> n;
+	long result = n;
+
+
+	for (long p = 2; p <= sqrt(n); p++) {
+		if (n % p == 0) {
+			result = result - result / p;
+			while (n % p == 0) {
+				n = n / p;
+			}
+		}
+	}
+
+	if (n > 1) {
+		result = result - result / n;
+	}
+
+	cout << result;
+
+	return 0;
+}
