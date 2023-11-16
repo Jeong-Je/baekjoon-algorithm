a948083dc7ad12c6c5790be605c2d9b22254308d
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2023-11-16 19:22:43 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 88 ms, Memory: 12488 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+static int n, m, r, seq=0;
+static vector< vector <int>> v;
+static vector<bool> visited;
+static stack<int> s;
+
+static vector<int> order;
+
+void DFS(int n);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n >> m >> r;
+	v.resize(n + 1);
+	order.resize(n + 1);
+	v.resize(n + 1);
+	visited = vector<bool>(n + 1, false);
+	int node1, node2;
+	for (int i = 1; i <= m; i++) {
+		cin >> node1 >> node2;
+		v[node1].push_back(node2);
+		v[node2].push_back(node1);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(v[i].begin(), v[i].end());
+	}
+	DFS(r);
+
+	for (int i = 1; i <= n; i++) {
+		cout << order[i] << "\n";
+	}
+}
+
+void DFS(int n) {
+	seq++;
+	order[n] = seq;
+	visited[n] = true;
+	for (int i : v[n]) {
+		if (!visited[i]) {
+			DFS(i);
+		}
+	}
+}
