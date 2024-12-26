c9d0b5fb7feaa3c8f650c1a87305a2e305470c73
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2024-12-26 19:43:49 +0900

    [Silver V] Title: K번째 수, Time: 700 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,21 +1,70 @@
 // 11004
+
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
+
+void quickSort(vector<int>& v, int s, int e, int k);
+int partition(vector<int>& v, int s, int e);
+void swap(vector<int>& v, int s, int e);
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n, k,data;
-	cin >> n >> k;
-	vector<int> v;
-	for (int i = 0; i < n; i++) {
-		cin >> data;
-		v.push_back(data);
-	}
-	sort(v.begin(), v.end());
+	cout.tie(NULL);
 
-	cout << v.at(k-1);
-	return 0;
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	quickSort(v, 0, n - 1, k - 1);
+	cout << v[k - 1];
+}
+
+void quickSort(vector<int>&v, int s, int e, int k) {
+	int pivot = partition(v, s, e);
+	if (pivot == k) {
+		return;
+	}
+	else if (k < pivot) {
+		quickSort(v, s, pivot - 1, k);
+	}
+	else {
+		quickSort(v, pivot + 1, e, k);
+	}
+}
+
+int partition(vector<int>& v, int s, int e) {
+	if (s + 1 == e) {
+		if (v[s] > v[e])swap(v, s, e);
+		return e;
+	}
+
+	int m = (s + e) / 2;
+	swap(v, s, m);
+	int pivot = v[s];
+	int i = s + 1;
+	int j = e;
+
+	while (i <= j) {
+		while (pivot < v[j] && j>0)j--;
+		while (pivot > v[i] && i < v.size()-1)i++;
+		if (i <= j)swap(v, i++, j--);
+	}
+	v[s] = v[j];
+	v[j] = pivot;
+
+	return j;
+}
+
+void swap(vector<int>& v, int s, int e) {
+	int temp = v[s];
+	v[s] = v[e];
+	v[e] = temp;
 }
