09af18eec6197ab8a34b9030e03c147b65440570
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-01-01 23:17:29 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 112 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,73 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+static vector<vector<int>> A;
+static vector<bool> visited;
+static vector<int> order;
+
+void BFS(int R);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, M, R;
+    cin >> N >> M >> R;
+    
+    A.resize(N+1);
+    order.resize(N+1, 0);
+    visited = vector<bool>(N+1, false);
+    
+    for(int i=0;i<M;i++){
+        int s, e;
+        cin >> s >> e;
+        A[s].push_back(e);
+        A[e].push_back(s);
+    }
+    
+    for(int i=1;i<=N;i++){
+        sort(A[i].begin(), A[i].end());
+    }
+    
+    BFS(R);
+    
+    for(int i=1;i<=N;i++){
+        cout<<order[i]<<"\n";
+    }
+
+    return 0;
+}
+
+void BFS(int R){
+    visited[R] = true;
+    
+    queue<int> q;
+    q.push(R);
+    
+    int index = 1;
+    
+    while(!q.empty()){
+        int now_node = q.front();
+        q.pop();
+        order[now_node] = index++;
+        for(int i: A[now_node]) {
+            if(!visited[i]){
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+}
