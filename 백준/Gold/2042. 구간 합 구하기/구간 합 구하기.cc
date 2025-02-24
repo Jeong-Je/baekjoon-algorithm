85e9ea6a8638cdf47b20113dd2f2471cba4febe9
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-02-25 00:53:13 +0900

    [Gold I] Title: 구간 합 구하기, Time: 192 ms, Memory: 18596 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,87 @@
+// 2042 구간 합 구하기 (https://www.acmicpc.net/problem/2042)
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+static vector<long> tree;
+
+void changeNode(int index, long val);
+void setTree(int i);
+long getSum(int s, int e);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N, M, K;
+    cin >> N >> M >> K;
+    
+    int treeHeight = 0;
+    int index = N;
+    
+    while(index != 0){
+        index /= 2;
+        treeHeight++;
+    }
+    
+    int treeSize = pow(2, treeHeight + 1);
+    
+    tree.resize(treeSize+1);
+    
+    int leftStartIndex = treeSize / 2 -1;
+    
+    
+    for(int i=leftStartIndex+1;i<=leftStartIndex+N;i++){
+        cin >> tree[i];
+    }
+
+    setTree(treeSize-1); // 트리의 구간 합 초기화
+
+    for(int i=0;i<M+K;i++){
+        long a, s, e;
+        cin >> a >> s >> e;
+        
+        if(a==1) { // 값 교체
+            changeNode(leftStartIndex+s, e);
+
+        } else { // 구간 합 출력
+            s += leftStartIndex;
+            e += leftStartIndex;
+            cout << getSum(s, e) << "\n";
+        }
+    }
+}
+
+long getSum(int s, int e) {
+    long partSum = 0;
+    while(s<=e){
+        if(s % 2 == 1) {
+            partSum += tree[s];
+            s++;
+        }
+        if(e % 2 == 0) {
+            partSum += tree[e];
+            e--;
+        }
+        s /= 2;
+        e /= 2;
+    }
+    return partSum;
+}
+
+void setTree(int i){
+    while(i != 1){
+        tree[i/2] += tree[i];
+        i--;
+    }
+}
+
+void changeNode(int index, long val){
+    long diff = val - tree[index];
+    
+    while(index > 0) {
+        tree[index] += diff;
+        index /= 2;
+    }
+}
