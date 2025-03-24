bb0f11da227f1d1dd3819e6bd074ed4850b1160c
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-03-24 11:18:38 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -5,44 +5,43 @@
 using namespace std;
 
 static int N, K;
-
 static bool visited[100001];
-static int dx[2] = {-1, 1};
+static int dx[2] = {-1 ,1};
 
-void BFS(int x);
+void BFS(int start);
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
     cin >> N >> K;
     
-    if(N==K){
-        cout << "0";
-        return 0;
+    if(N==K) {
+        cout << 0;
+    } else {
+        BFS(N);
     }
     
-    BFS(N);
 }
 
-void BFS(int x) {
-    visited[x] = true;
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, 1));
-    while(!q.empty()){
 
-        int now = q.front().first;
-        int cnt = q.front().second;
-        q.pop();
+void BFS(int start) {
+    visited[start] = true;
+    queue<pair<int, int>> que;
+    que.push({start, 1});
+    
+    while(!que.empty()){
+        int now = que.front().first;
+        int cnt = que.front().second;
+        que.pop();
+        
         for(int i=0;i<3;i++){
             int next_x;
-            if(i==2){
+            if(i==2) {
                 next_x = now * 2;
-            }else {
+            } else {
                 next_x = now + dx[i];
             }
-            if(next_x > -1 && next_x < 100001 && !visited[next_x]){
-                q.push(make_pair(next_x, cnt +1));
+            
+            if(next_x > -1 && next_x <100001 && !visited[next_x]) {
+                que.push({next_x, cnt+1});
                 visited[next_x] = true;
                 if(next_x == K) {
                     cout << cnt;
