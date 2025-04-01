086100ab931a92da2b0e4707f35141da9fffc120
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-04-01 23:20:09 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+using ll = long long;
+
+static ll dp[1000001];
+
+int main(){
+    int N;
+    cin >> N;
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    
+    for(int i=3;i<=N;i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+    }
+    
+    cout << dp[N];
+    
+}
