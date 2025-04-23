501db477860c3a0b46818a2b88f8fed69e0af37b
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-04-24 04:06:13 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,55 +1,48 @@
-// 1253
-
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n,data;
-	cin >> n;
-	vector<int> v;
-
-	for (int i = 0; i < n; i++) {
-		cin >> data;
-		v.push_back(data);
-	}
-
-	sort(v.begin(), v.end());
-
-	int cnt = 0;
-
-	for (int k = 0; k < n; k++) {
-		long find = v.at(k);
-		int i = 0;
-		int j = n - 1;
-
-		while (i < j) {
-			if (v.at(i) + v.at(j) == find) {
-				if (i != k && j != k) {
-					cnt++; break;
-				}
-				else if (i == k) {
-					i++;
-				}
-				else if (j == k) {
-					j--;
-				}
-			}
-			else if (v.at(i) + v.at(j) < find) {
-				i++;
-			}
-			else {
-				j--;
-			}
-		}
-	}
-	
-
-	cout << cnt;
-	return 0;
+int main(){
+    int N;
+    cin >> N;
+    
+    vector<int> v(N);
+    
+    for(int i=0;i<N;i++){
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
+    
+    int count = 0;
+    
+    for(int i=0;i<N;i++){
+        int target = v[i];
+        
+        int left = 0;
+        int right = N-1;
+        
+        while(left < right) {
+            if(v[left] + v[right] == target) {
+                if(left != i && right != i) {
+                    count++; 
+                    break;
+                }
+                else if(left == i) {
+                    left++;
+                } else if(right == i) {
+                    right--;
+                } 
+            } else if (v[left] + v[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+            
+        }
+    }
+    
+    cout << count;
 }
