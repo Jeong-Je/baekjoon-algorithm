2aecb07f42c48bb58b3afc0beedd8b507e7ac6d9
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-05-17 17:52:52 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 72 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -21,7 +21,7 @@ int main(){
     int right = 1;
     check[v[left]]++;
     
-    while(left < right && right <= N-1) {
+    while(right <= N-1) {
         if(check[v[right]] < K) { // 같은 정수가 K개 미만 
             check[v[right]]++;
         } else { // 같은 정수가 K개 이상 
