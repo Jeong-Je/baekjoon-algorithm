1b16bad72d5643365291b9da795b4e318e283e35
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-07-12 17:14:45 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -1,55 +1,58 @@
 #include <iostream>
-#include <deque>
+#include <vector>
+
 using namespace std;
 
-int durability[201];
-bool robot_exist[201];
+static int durability[201];
+static bool robot_exist[201];
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-
+    
     int N, K;
     cin >> N >> K;
-
-    for (int i = 1; i <= 2 * N; i++) {
+    
+    for(int i=1;i<=2*N;i++){
         cin >> durability[i];
     }
-
-    int step = 0;
-    int zero_count = 0;
-
-    while (zero_count < K) {
-        step++;
-
-        // 1. 벨트와 로봇 회전
-        int last_durability = durability[2 * N];
-        for (int i = 2 * N; i >= 2; i--) {
-            durability[i] = durability[i - 1];
-            robot_exist[i] = robot_exist[i - 1];
+    
+    int durability_zero_count = 0;
+    int result = 0;
+    while(durability_zero_count < K){
+        result++;
+        
+        
+        // 1.벨트 회전
+        int tmp_d = durability[2*N];
+        for (int i=2*N;i>=2;i--) {
+            durability[i] = durability[i-1];
+            robot_exist[i] = robot_exist[i-1];
         }
-        durability[1] = last_durability;
+        durability[1] = tmp_d;
         robot_exist[1] = false;
-        robot_exist[N] = false; // 내리는 위치
-
-        // 2. 로봇 이동
-        for (int i = N - 1; i >= 1; i--) {
-            if (robot_exist[i] && !robot_exist[i + 1] && durability[i + 1] > 0) {
-                robot_exist[i] = false;
-                robot_exist[i + 1] = true;
-                durability[i + 1]--;
-                if (durability[i + 1] == 0) zero_count++;
-            }
+        robot_exist[N] = false; // 내리는 칸에서 로봇 내리기
+        
+        
+        // 2. 로봇 움직이기
+        for(int i=N-1;i>=1;i--){
+           if(robot_exist[i] && !robot_exist[i+1] && durability[i+1]) {
+               robot_exist[i+1] = true;
+               robot_exist[i] = false;
+               durability[i+1]--;
+               if(durability[i+1] == 0) durability_zero_count++;
+           }
         }
-        robot_exist[N] = false; // 내리는 위치
-
-        // 3. 로봇 올리기
-        if (!robot_exist[1] && durability[1] > 0) {
+        robot_exist[N] = false;
+        
+        // 3. 로봇 올리기 
+        if(robot_exist[1] == false && durability[1] != 0) { 
             robot_exist[1] = true;
-            durability[1]--;
-            if (durability[1] == 0) zero_count++;
-        }
+            durability[1]--; 
+            if(durability[1] == 0) durability_zero_count++;
+        } 
     }
-
-    cout << step;
+    
+    
+    cout << result;
 }
