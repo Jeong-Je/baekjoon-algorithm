5a63fb382ccc58ad9ec473c4d78186a46105ccd3
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-08-15 12:55:38 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+static int dp[10001][4];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T;
+    cin >> T;
+    
+    
+    dp[0][1] = 1;
+    dp[1][1] = 1;
+    
+    for(int i=2;i<=10000;i++)
+    {
+        dp[i][1] = dp[i-1][1];
+        dp[i][2] = dp[i-2][1] + dp[i-2][2];
+        if(i >= 3)
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+    }
+    
+    for(int i=0;i<T;i++)
+    {
+        int n;
+        cin >> n;
+        
+        cout << dp[n][1] + dp[n][2] + dp[n][3] << "\n";
+    }
+}
