5ad8204911d31469f9e46f5b869cc975d726c235
Jeong-Je <131790445+Jeong-Je@users.noreply.github.com>
2025-09-08 18:24:21 +0900

    [Gold IV] Title: 푸앙이와 러닝머신, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/25597. 푸앙이와 러닝머신/푸앙이와 러닝머신.cc
+++ 백준/Gold/25597. 푸앙이와 러닝머신/푸앙이와 러닝머신.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     int X, T;
-    int four_meter_time, eight_meter_time, remain;
+    int four_meter_time, eight_meter_time, one_meter_time;
     cin >> X >> T;
     
     /////////////////////////////////////////////////////////////////////////
@@ -40,10 +40,10 @@ int main()
     
     // 2-1. 1미터랑 4미터 조합 확인하기
     four_meter_time = X / 4; // 4미터 속도로 달리는 시간 
-    remain = X % 4;          // 1미터 속도로 달리는 시간 
-    if(four_meter_time + remain <= T)
+    one_meter_time = X % 4;          // 1미터 속도로 달리는 시간 
+    if(four_meter_time + one_meter_time <= T)
     {
-        int start_time = T - (four_meter_time + remain);
+        int start_time = T - (four_meter_time + one_meter_time);
         cout << "2\n";
         cout << start_time << " 4\n";
         cout << start_time + four_meter_time << " 1\n";
@@ -53,10 +53,10 @@ int main()
     
     // 2-2. 1미터랑 8미터 조합 확인하기 
     eight_meter_time = X / 8; // 8미터 속도로 달리는 시간 
-    remain = X % 8;           // 1미터 속도로 달리는 시간 
-    if(eight_meter_time + remain <= T)
+    one_meter_time = X % 8;           // 1미터 속도로 달리는 시간 
+    if(eight_meter_time + one_meter_time <= T)
     {
-        int start_time = T - (eight_meter_time + remain);
+        int start_time = T - (eight_meter_time + one_meter_time);
         cout << "2\n";
         cout << start_time << " 8\n";
         cout << start_time + eight_meter_time << " 1\n";
@@ -67,39 +67,34 @@ int main()
     // 2-3. 4미터랑 8미터 조합 확인하기 
     eight_meter_time = X / 8;
     four_meter_time = (X % 8) / 4;
-
-        // 아래 식이 나누어 떨어져야 4미터 8미터 조합 가능 
-        if((X % 8) % 4 == 0)
+    one_meter_time = (X % 8) % 4;
+    // 아래 식이 나누어 떨어져야 4미터 8미터 2개로 조합 가능 
+    if(one_meter_time == 0)
+    {
+        if(eight_meter_time + four_meter_time <= T)
         {
-            if(eight_meter_time + four_meter_time <= T)
-            {
-                int start_time = T - (eight_meter_time + four_meter_time);
-                cout << "2\n";
-                cout << start_time << " 8\n";
-                cout << start_time + eight_meter_time << " 4\n";
-                return 0;
-            }
-        }        
-
-    
-    
+            int start_time = T - (eight_meter_time + four_meter_time);
+            cout << "2\n";
+            cout << start_time << " 8\n";
+            cout << start_time + eight_meter_time << " 4\n";
+            return 0;
+        }
+    }        
     /////////////////////////////////////////////////////////////////////////
+    //
     // 3. 1미터, 4미터, 8미터 버튼 세 개를 모두 써야 되는 경우  
-    eight_meter_time = X / 8;
-    four_meter_time = (X % 8) / 4;
-    remain = (X % 8) % 4;
-    
-
-        if(eight_meter_time + four_meter_time + remain <= T)
+    else
+    {
+       if(eight_meter_time + four_meter_time + one_meter_time <= T)
         {
-            int start_time = T - (eight_meter_time + four_meter_time + remain);
+            int start_time = T - (eight_meter_time + four_meter_time + one_meter_time);
             cout << "3\n";
             cout << start_time << " 8\n";
             cout << start_time + eight_meter_time << " 4\n";
             cout << start_time + eight_meter_time + four_meter_time << " 1\n";
             return 0;
-        }
-    
+        } 
+    }
 
     
     /////////////////////////////////////////////////////////////////////////
