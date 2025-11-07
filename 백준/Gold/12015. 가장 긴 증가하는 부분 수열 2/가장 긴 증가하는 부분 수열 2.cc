6464f5877d158a70e8fe90067b31d4c2363369c7
SON JEONG JE <jeongjeson656@gmail.com>
2025-11-07 14:13:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 452 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> arr, ans;
+
+int binary(int key)
+{
+    int left = 0, right = ans.size() - 1;
+    
+    while(left < right)
+    {
+        int mid = (left + right) / 2;
+        
+        if(key > ans[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return right;
+}
+
+int main()
+{
+    cin >> N;
+    
+    arr.resize(N);
+    for(int i=0;i<N;i++)
+    {
+        cin >> arr[i];
+    }
+    
+    ans.push_back(arr[0]);
+    
+    for(int i=1;i<N;i++)
+    {
+        if(ans.back() < arr[i])
+        {
+            ans.push_back(arr[i]);
+        }
+        else
+        {
+            int idx = binary(arr[i]);
+            ans[idx] = arr[i];
+        }
+    }
+    
+    cout << ans.size();
+}
