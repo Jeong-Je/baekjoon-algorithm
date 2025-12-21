7a1cfd19ff0ffc620e8c4ec0a02ac1cacdc7eabe
SON JEONG JE <jeongjeson656@gmail.com>
2025-12-21 13:53:26 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int alpha[26];
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    for(int i=0;i<s.size();i++)
+    {
+        alpha[s[i] - 'a']++;
+    }
+    
+    
+    for(int i=0;i<26;i++)
+    {
+        cout << alpha[i]<< " ";
+    }
+}
