6d3c4b5e9549cfe40bf522353f25b5a8f90bc403
SON JEONG JE <jeongjeson656@gmail.com>
2025-12-26 12:38:21 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -3,37 +3,56 @@
 
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    int N;
-    cin >> N;
-    
-    // { 값 , 인덱스 } 
-    stack<pair<int, int>> stk;
-    
-    for(int i=1;i<=N;i++){
-        int h;
-        cin >> h;
-        
-        if(stk.empty()) {
-            cout << "0 ";
-            stk.push({h, i});
-        } else {
-            while(!stk.empty()) {
-                auto [value, index] = stk.top();
-                //cout << value << " " << index << endl;
-                
-                if(value > h){ 
-                    cout << index << " ";
-                    break;
-                } else {
-                    stk.pop();
-                }
-            }
-            
-            if(stk.empty()) cout << "0 ";
-            stk.push({h, i});
-        }
-    }
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	stack<pair<int,int>> stack;
+
+	for (int i = 1; i <= N; i++)
+	{
+		int height;
+		cin >> height;
+
+		// 스택이 비어 있으면 
+		if (stack.empty() == true)
+		{
+			cout << "0 ";
+			stack.push({height, i});
+		}
+
+		else
+		{
+			int s_top_height = stack.top().first;
+			int s_top_index = stack.top().second;
+
+			if (s_top_height >= height)
+			{
+				cout << s_top_index << ' ';
+			}
+
+			else
+			{
+				while (stack.empty() == false && stack.top().first < height)
+				{
+					stack.pop();
+				}
+
+				if (stack.empty())
+				{
+					cout << "0 ";
+				}
+				else
+				{
+					cout << stack.top().second << ' ';
+				}
+			}
+
+			stack.push({ height, i });
+		}
+	}
 }
