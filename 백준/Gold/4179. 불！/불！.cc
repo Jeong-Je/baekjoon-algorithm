c760b5c83f9434c3b7e1025b86d362f04fb98e89
SON JEONG JE <jeongjeson656@gmail.com>
2025-12-30 10:26:20 +0900

    [Gold III] Title: 불!, Time: 64 ms, Memory: 6908 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,93 +1,98 @@
 #include <iostream>
 #include <queue>
 
-using namespace std;
-
-static int map[1001][1001];
-static int visited[1001][1001];
-int di[4] = {-1, 1, 0, 0};
-int dj[4] = {0, 0, -1, 1};
-
-queue<pair<int, int>> q1;
-queue<pair<int, int>> q2;
+using namespace  std;
 
 int R, C;
 
-void bfs(int j_i, int j_j);
+queue<pair<int, int>> fire_que;
+queue<pair<int, int>> jihu_que;
 
-int main()
-{
-   cin >> R >> C;
-   
-   int start_i, start_j;
-   int fire_i, fire_j;
-   for(int i=0;i<R;i++)
-   {
-       for(int j=0;j<C;j++)
-       {
-           char c;
-           cin >> c;
-           if(c == '#') map[i][j] = 1;
-           else if(c == '.') map[i][j] = 0;
-           else if(c == 'J') {start_i = i; start_j = j;} 
-           else {q2.push({i, j}); map[i][j] = 2;}
-       }
-   }
-   
-   
-   bfs(start_i, start_j);
-}
+char map[1000][1000];
+int visited[1000][1000];
 
-void bfs(int j_i, int j_j)
-{
-    visited[j_i][j_j] = 1;
-    
-    q1.push({j_i, j_j});
-    
-while(!q1.empty())
-{
-    // 1. 불 확산
-    int q2_size = q2.size();
-    for(int i=0;i<q2_size;i++) {
-        auto [f_now_i, f_now_j] = q2.front();
-        q2.pop();
-        for(int d=0;d<4;d++) {
-            int f_next_i = f_now_i + di[d];
-            int f_next_j = f_now_j + dj[d];
-            if(f_next_i < 0 || f_next_i >= R || f_next_j < 0 || f_next_j >= C) continue;
-            if(map[f_next_i][f_next_j] == 0) {
-                map[f_next_i][f_next_j] = 2;
-                q2.push({f_next_i, f_next_j});
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, -1, 1};
+
+int start_r, start_c;
+
+void bfs(int r, int c);
+
+int main() {
+    cin >> R >> C;
+
+    for (int i=0;i<R;i++) {
+        for (int j=0;j<C;j++) {
+            char c;
+            cin >> c;
+
+            if (c=='J') {
+                start_r = i;
+                start_c = j;
+                map[i][j] = '.';
+            } else if (c == 'F') {
+                fire_que.push({i, j});
+                map[i][j] = 'F';
+            } else if (c == '#') {
+                map[i][j] = '#';
+            } else {
+                map[i][j] = '.';
             }
         }
     }
 
-    // 2. 지훈 이동
-    int q1_size = q1.size();
-    for(int i=0;i<q1_size;i++) {
-        auto [j_now_i, j_now_j] = q1.front();
-        q1.pop();
-
-        for(int d=0; d<4; d++) {
-            int j_next_i = j_now_i + di[d];
-            int j_next_j = j_now_j + dj[d];
-
-            // 탈출 성공
-            if(j_next_i < 0 || j_next_i >= R || j_next_j < 0 || j_next_j >= C) {
-                cout << visited[j_now_i][j_now_j];
-                return;
-            }
-
-            if(visited[j_next_i][j_next_j]) continue;
-            if(map[j_next_i][j_next_j] != 0) continue; // 벽 or 불이면 못 감
-
-            visited[j_next_i][j_next_j] = visited[j_now_i][j_now_j] + 1;
-            q1.push({j_next_i, j_next_j});
-        }
-    }
+    bfs(start_r, start_c);
 }
 
-    
-    
+void bfs(int r, int c) {
+    jihu_que.push({r, c});
+    visited[r][c] = 1;
+
+    while (!jihu_que.empty()) {
+        // 1. 불 먼저 확산 !
+        int fire_size = fire_que.size();
+        for (int i=0;i<fire_size;i++) {
+            auto [now_i, now_j] = fire_que.front();
+            fire_que.pop();
+
+            for (int d=0;d<4;d++) {
+                int next_i = now_i + dr[d];
+                int next_j = now_j + dc[d];
+
+                if (next_i < 0 || next_i >= R || next_j < 0 || next_j >= C) continue;
+                if (map[next_i][next_j] != '.') continue;
+
+                map[next_i][next_j] = 'F';
+                fire_que.push({next_i, next_j});
+            }
+        }
+
+        // 2. 지훈 이동 !
+        int jihu_size = jihu_que.size();
+        for (int i=0;i<jihu_size;i++) {
+            auto [now_i, now_j] = jihu_que.front();
+            jihu_que.pop();
+
+            for (int d=0;d<4;d++) {
+                int next_i = now_i + dr[d];
+                int next_j = now_j + dc[d];
+
+                // 탈출 성공 !
+                if (next_i < 0 || next_i >= R || next_j < 0 || next_j >= C) {
+                    cout << visited[now_i][now_j];
+                    return;
+                }
+
+                // 이미 방문했거나 길이 아니면 이동 불가
+                if (visited[next_i][next_j] || map[next_i][next_j] != '.') continue;
+
+                visited[next_i][next_j] = visited[now_i][now_j] + 1;
+                jihu_que.push({next_i, next_j});
+            }
+        }
+
+    }
+
+
     cout << "IMPOSSIBLE";
 }
